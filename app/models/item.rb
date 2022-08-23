7f0c487b5666ff02f7dfe797bff721106ea12216
Viktor Kozma <kozmaviktor1992@gmail.com>
2022-08-23 09:58:02 +0800

    Fix: devise registration and login updated and tested - user creation and log in now functional

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,6 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 6 }
   validates :category, presence: true
-  validates :category, inclusion: { in: ["Vehicles", "Tools", "Clothing", "Weddings", "Electronics", "Jewellery", "Bags", "Books", "Rooms", "Sports", "Boats", "Aeroplains"] }
+  validates :category, inclusion: { in: ["Vehicles", "Tools", "Clothing", "Weddings", "Electronics", "Jewellery", "Bags", "Books", "Rooms", "Sports", "Boats", "Planes"] }
   validates :price_per_day, presence: true, numericality: true
 end
