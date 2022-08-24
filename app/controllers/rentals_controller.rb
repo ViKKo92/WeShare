99d8242d03601a67ade9a53364690e2e73465f6d
Viktor Kozma <kozmaviktor1992@gmail.com>
2022-08-24 20:28:09 +0800

    Created rental CRUD - routing not working, style not added yet

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,36 @@
 class RentalsController < ApplicationController
+  def index
+    @rentals = Rental.where(user: current_user)
+  end
+  
+  def show
+    @rental = Rental.find(params[:id])
+  end
+  
+  def new
+    @item = Item.find(params[:item_id])
+    @rental = Rental.new()
+  end
+  
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.user = current_user
+    if @rental.save
+      redirect_to item_rentals
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    @rental = Rental.find(params[:id])
+    @rental.destroy
+    redirect_to item_rentals, status: :see_other
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date)
+  end
 end
